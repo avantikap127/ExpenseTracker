0ac049044e052f25be425d55ac81ed0ad0275df0
Avantika Pandey <153753094+avantikap127@users.noreply.github.com>
2026-03-08 01:13:45 +0530

    Update UserRepository.java

--- backend/src/main/java/com/expensetracker/repository/UserRepository.java
+++ backend/src/main/java/com/expensetracker/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.expensetracker.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.expensetracker.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 
 }

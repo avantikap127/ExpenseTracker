ca780bd09a7ec1d12838b7f0a043d60904abe50b
Avantika Pandey <153753094+avantikap127@users.noreply.github.com>
2026-03-07 12:40:50 +0530

    Update Dashboard.js

--- Frontend/src/components/Dashboard.js
+++ Frontend/src/components/Dashboard.js
@@ -34,13 +34,13 @@ function Dashboard() {
   }, []);
 
   const loadDashboard = () => {
-    axios.get("http://https://expense-tracker-wheat-theta.vercel.app//api/expenses/dashboard")
+    axios.get("https://expense-tracker-wheat-theta.vercel.app//api/expenses/dashboard")
       .then(res => setSummary(res.data))
       .catch(err => console.log(err));
   };
 
   const loadExpenses = () => {
-    axios.get("http://https://expense-tracker-wheat-theta.vercel.app//api/expenses")
+    axios.get("https://expense-tracker-wheat-theta.vercel.app//api/expenses")
       .then(res => setExpenses(res.data))
       .catch(err => console.log(err));
   };
@@ -50,14 +50,14 @@ function Dashboard() {
     if (!categoryFilter) return;
 
     axios
-      .get(`http://https://expense-tracker-wheat-theta.vercel.app//api/expenses/category/${categoryFilter}`)
+      .get(`https://expense-tracker-wheat-theta.vercel.app//api/expenses/category/${categoryFilter}`)
       .then(res => setFilteredExpenses(res.data))
       .catch(err => console.log(err));
   };
 
   const deleteExpense = (id) => {
 
-    axios.delete(`http://https://expense-tracker-wheat-theta.vercel.app//api/expenses/${id}`)
+    axios.delete(`https://expense-tracker-wheat-theta.vercel.app//api/expenses/${id}`)
       .then(() => {
         loadExpenses();
         loadDashboard();
@@ -73,7 +73,7 @@ function Dashboard() {
       return;
     }
 
-    axios.post("http://https://expense-tracker-wheat-theta.vercel.app//api/expenses", {
+    axios.post("https://expense-tracker-wheat-theta.vercel.app//api/expenses", {
       description: form.description,
       amount: Number(form.amount),
       date: form.date,
@@ -471,3 +471,4 @@ const tableStyle = {
 
 
 export default Dashboard;
+

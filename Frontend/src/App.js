f7176b4c8feec588b656e50ae6883b48f475a9da
Avantika Pandey <153753094+avantikap127@users.noreply.github.com>
2026-03-08 00:15:06 +0530

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,18 @@
+import React, { useState } from "react";
 import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
 import "./App.css";
 
 function App() {
+
+  const [loggedIn, setLoggedIn] = useState(
+    localStorage.getItem("token") !== null
+  );
+
+  if (!loggedIn) {
+    return <Login setLoggedIn={setLoggedIn} />;
+  }
+
   return <Dashboard />;
 }
 
